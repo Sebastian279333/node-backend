ae833c6b40f88afb5fe5f0f0b143386c16027744
Admin <admin@example.com>
2025-12-19 17:02:43 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from our API" });
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
